9941144a944ed70b54c6995bf94d4bcff2444cb6
Clotia Johari <freewillandjustice@gmail.com>
2022-01-07 16:13:48 -0500

    added EO links and UL

--- pages/esperanto.jsx
+++ pages/esperanto.jsx
@@ -2,8 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import eostar from "../public/eostar.png";
 import styles from "../styles/Esperanto.module.css";
+import Link from "next/link";
 
 const Esperanto = () => {
+  const links = [
+    <Link href="http://lernu.net">Lernu.net</Link>,
+    <Link href="https://www.duolingo.com">Duolingo</Link>,
+    <Link href="https://www.reddit.com/r/Esperanto/">Esperanto Reddit</Link>,
+    <Link href="https://www.youtube.com/watch?v=UU0TCz9DwXs">Evildea</Link>,
+  ];
+
   return (
     <div>
       <Head>
@@ -14,27 +22,39 @@ const Esperanto = () => {
       <div className={styles.container}>
         <div className={styles.card}>
           <h1>
-            <span> EO:</span> La Esperanto Lingvo
+            <span>
+              {" "}
+              EO: La Internacia Lingvo <br />
+              (The International Language)
+            </span>
           </h1>
           <p className={styles.blurb}>
             Esperanto is a constructed language created by L.L. Zamenhof. It's
             many times easier to learn than any 'natural' language and acts as a
             bridge of communication between individuals who speak two different
             native tongues.
+            <h2>Links to free learning materials and resources: </h2>
           </p>
-          <p> ESPERANTO CONTENT WILL BE ADDED, SOON.</p>
-        </div>
-        <div className={styles.card}>
-          <Image
-            src={eostar}
-            alt="eo flag"
-            width="75%"
-            height="75%"
-            layout="fill"
-            objectFit="contain"
-            alt="eo star"
-          />
-        </div>
+          <ul>
+            {links.map((link) => (
+              <li key={link}>{link}</li>
+            ))}
+          </ul>
+        </div>{" "}
+        <p className={styles.blurb}>
+          More Esperanto content and links to be added, soon.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <Image
+          src={eostar}
+          alt="eo flag"
+          width="75%"
+          height="75%"
+          layout="fill"
+          objectFit="contain"
+          alt="eo star"
+        />
       </div>
     </div>
   );
