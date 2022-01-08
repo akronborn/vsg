dcdf5d3ad57c7c247ad6f6aff0032079cbc976ba
Clotia Johari <freewillandjustice@gmail.com>
2022-01-07 19:37:29 -0500

    consolidated footer divs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,28 +9,24 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.primary}>
         <h1 className={styles.title}>Verda Stelo Gaming</h1>
-        <h1 className={styles.link}>
+        <div className={styles.link}>
           <a href="/contact" passHref>
             <span className={styles.reach}>
               Post an event or just reach out
             </span>
             <Image src={button} alt="link button" width="40px" height="40px" />
           </a>
-        </h1>
-      </div>
-      <div className={styles.secondary}>
-        <div className={styles.item}>
-          Links: <br />
-          <Link href="https://www.linkedin.com/in/akronborn/">
-            Linkedin
-          </Link> | <Link href="https://github.com/akronborn"> Git </Link>
         </div>
         <div className={styles.item}>2022 AkrnBrn</div>
       </div>
+
       <div className={styles.secondary}>
         <Image src={contact} alt="contact button" width="40px" height="40px" />
         <div className={styles.item}>
-          FreewillandJustice@gmail.com <br /> (586)209-8832
+          FreewillandJustice@gmail.com <br /> (586)209-8832 <br />
+          <Link href="https://www.linkedin.com/in/akronborn/">
+            Linkedin
+          </Link> | <Link href="https://github.com/akronborn"> Git </Link>
         </div>
       </div>
     </div>
