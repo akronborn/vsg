a37e40a19fc47ba9f0c845c38521aea96aed025a
Clotia Johari <freewillandjustice@gmail.com>
2022-12-31 13:15:54 -0500

    shift slider

--- components/SimpleSlider.jsx
+++ components/SimpleSlider.jsx
@@ -33,13 +33,13 @@ export default class Resizable extends Component {
         >
           <Slider {...settings}>
             <div>
+              <Image src={gaming3} alt="Android photo" />
+            </div>
+            <div>
               <Image src={gaming1} alt="XBox photo" />
             </div>
             <div>
               <Image src={gaming2} alt="Playstation photo" />
-            </div>
-            <div>
-              <Image src={gaming3} alt="Android photo" />
             </div>
             <div>
               <Image src={gaming4} alt="Board Game photo" />
