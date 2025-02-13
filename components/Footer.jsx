f953ce7af95fef0181c49b404c24d8095c609bf9
Akronborn <freewillandjustice@gmail.com>
2025-02-13 13:30:47 -0500

    height tagline footer color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
         <div className={styles.item}>
           <div className={styles.info}>
-            <Image src={button} alt="link icon" width="7px" height="7px" />
+            <Image src={button} alt="link icon" width="2px" height="2px" />
             <Link href="https://www.linkedin.com/in/akronborn/">
               Linkedin
             </Link>| <Link href="https://github.com/akronborn"> Github </Link>
