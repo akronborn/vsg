f3d0bdef24c7dcfe8dfa7c2bf97051c843833845
Akronborn <freewillandjustice@gmail.com>
2025-02-14 16:14:20 -0500

    added <b>

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -25,7 +25,8 @@ const Schedule = () => {
             <h2>Non-VSG Events</h2>
             <p className={styles.p}>
               <b> Virtual/Online Events: </b> <br></br>
-              U.S. Monata Kunveno: March, 25th, Harold Washing Library, Chicago, IL <br></br>
+              <b> U.S. Monata Kunveno</b> <br></br>
+              March, 25th, Harold Washing Library, Chicago, IL <br></br>
               <b>World Esperanto Congress </b><br></br>
               110th World Esperanto Congress, Brno, Czech Republic
             </p>
